e7bd89d1e918df903e3465c2b81bbeb8ebb3add0
Kuba Jaroszewski <jakub.jaroszewski@gmail.com>
2023-04-18 11:17:26 +0200

    Add stackdriver tracing formatter

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
+use ambient_proxy_manager::telemetry::init_subscriber;
 use std::net::{SocketAddr, TcpListener};
-use ambient_proxy_manager::telemetry::{get_subscriber, init_subscriber};
-
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let subscriber = get_subscriber(
+    init_subscriber(
         "ambient_proxy_manager".into(),
         "info".into(),
         std::io::stdout,
     );
-    init_subscriber(subscriber);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     let listener = TcpListener::bind(addr)?;
--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,5 +1,8 @@
-use prometheus_client::{encoding::EncodeLabelSet, metrics::{counter::Counter, family::Family}};
-use tracing::{subscriber::set_global_default, Subscriber};
+use prometheus_client::{
+    encoding::EncodeLabelSet,
+    metrics::{counter::Counter, family::Family},
+};
+use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
@@ -18,34 +21,44 @@ pub struct Metrics {
 impl Metrics {
     pub fn new_registery(registry: &mut prometheus_client::registry::Registry) -> Self {
         let proxy_requests = Family::default();
-        registry.register("proxy_requests", "Count of /proxy requests", proxy_requests.clone());
+        registry.register(
+            "proxy_requests",
+            "Count of /proxy requests",
+            proxy_requests.clone(),
+        );
         Self { proxy_requests }
     }
 
     pub fn inc_proxy_requests(&self, country: String, ambient_version: String) {
-        tracing::debug!("Incrementing proxy request counter for country: {}, ambient_version: {}", country, ambient_version);
-        self.proxy_requests.get_or_create(&ProxyRequestLabels { country, ambient_version }).inc();
+        tracing::debug!(
+            "Incrementing proxy request counter for country: {}, ambient_version: {}",
+            country,
+            ambient_version
+        );
+        self.proxy_requests
+            .get_or_create(&ProxyRequestLabels {
+                country,
+                ambient_version,
+            })
+            .inc();
     }
 }
 
-pub fn get_subscriber<Sink>(
-    name: String,
-    env_filter: String,
-    sink: Sink,
-) -> impl Subscriber + Send + Sync
+pub fn init_subscriber<Sink>(name: String, env_filter: String, sink: Sink)
 where
     Sink: for<'a> tracing_subscriber::fmt::MakeWriter<'a> + Send + Sync + 'static,
 {
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name, sink);
-    Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-}
 
-pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
     LogTracer::init().expect("Failed to set logger");
-    set_global_default(subscriber).expect("Failed to set subscriber");
+
+    let registry = Registry::default().with(env_filter);
+    if std::env::var("LOG_FORMAT").map(|v| v.to_lowercase()) == Ok("bunyan".to_string()) {
+        let formatting_layer = BunyanFormattingLayer::new(name, sink);
+        set_global_default(registry.with(JsonStorageLayer).with(formatting_layer))
+    } else {
+        set_global_default(registry.with(tracing_stackdriver::layer().with_writer(sink)))
+    }
+    .expect("Failed to set subscriber");
 }
